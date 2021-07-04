4ab724ed40272544353302131e70ee8a11686cae
gyan0890 <gyanlakshmi@gmail.com>
2021-07-04 19:37:13 +0530

    NEO Hackathon

---
+++ smartcontracts/hello_world.py
@@ -0,0 +1,11 @@
+from boa3.builtin import NeoMetadata, metadata, public
+from boa3.builtin.interop.storage import put, get
+
+@public
+def Main() -> bool:
+    put('hello', 'world')
+    return True
+
+@public
+def hello() -> str:
+    return bytes.to_str(get("Hello"))
